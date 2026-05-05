82ecad358596b46ecab9b6f21f136287153945d1
Paul Quiring <paul.s.quiring@gmail.com>
2026-05-05 13:10:30 -0700

    add sitemap and robots

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://pdxgaragepros.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://pdxgaragepros.com/garage-door-repair-portland",
+      lastModified: new Date(),
+    },
+  ];
+}
